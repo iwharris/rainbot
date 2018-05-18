46e747be7c610d9fc0053cf9d46e9566f808d97e
Ian Harris <ian.harris@appdirect.com>
2018-05-18 13:55:41 -0600

    Return 204 when a browser requests favicon

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,6 @@ router.get('/', (req, res) => res.redirect('/health'));
 
 router.get('/health', (req, res) => res.send('OK'));
 
+router.get('/favicon.ico', (req, res) => res.status(204));
+
 export default router;
